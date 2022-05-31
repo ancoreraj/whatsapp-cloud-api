bba1815b2a08cfcfb337f66a37d910134d9195d9
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-05-31 21:35:55 +0530

    Added constants

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const GraphAPIBaseUrl: string = "https://graph.facebook.com/v13.0";
+
+export enum MessageTypes {
+    TEXT = "text",
+    IMAGE = "image",
+    DOCUMENT = "document",
+    AUDIO = "audio",
+    VIDEO = "video",
+    STICKER = "sticker",
+};
